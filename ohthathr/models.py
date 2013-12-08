7d68323774dd5fa86e17437c6ef34ea3cf84b84c
andrewshkovskii <andrewshkovskiI@gmail.com>
2013-12-08 18:50:43 +0400

    Поле description базовой модели теперь not null;

--- ohthathr/models.py
+++ ohthathr/models.py
@@ -9,13 +9,13 @@ from taggit.managers import TaggableManager
 class Base(models.Model):
     """Базовая модель для все моделей в системе."""
     title = models.CharField(u"название", max_length=255)
+    company = models.CharField(u"Компания, к которой относится %(class)s", max_length=255)
     url = models.URLField(u"ссылка", null=True, blank=True)
     rating = models.IntegerField(verbose_name=u"рейтинг", default=0)
     added_by = models.ForeignKey(User, verbose_name=u"добавил")
     date = models.DateTimeField(auto_now_add=True, verbose_name=u"дата добавления")
     last_edit = models.DateTimeField(auto_now=True, verbose_name=u"дата последнего изменения")
-    description = models.TextField(null=True, blank=True)
-    company = models.CharField(u"Компания, к которой относится %(class)s", max_length=255)
+    description = models.TextField(verbose_name=u"Описание")
     tags = TaggableManager(verbose_name=u"теги", blank=True)
     deleted = models.BooleanField(verbose_name=u"удалено?", default=False)
     comments = GenericRelation(Comment)
